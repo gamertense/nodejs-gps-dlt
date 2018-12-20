97e60489373f504746d14ce84091f85b7f965ebe
Antonio Jack <ict2jack@gmail.com>
2018-12-21 02:33:38 +0700

    First push

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+// app.get('/', (request, response) => {
+//     response.json({ info: 'Node.js, Express, and Postgres API' })
+// })
+
+app.get('/', db.getCarTrack)
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
+
+// var minutes = 2, the_interval = minutes * 60 * 1000;
+// setInterval(function () {
+//     console.log("I am doing my 1 minute check");
+//     postReq(db.getCarTrack())
+// }, the_interval);
---
+++ queries.js
@@ -0,0 +1,89 @@
+let pg = require('pg')
+const moment = require('moment');
+const config = require('./config')
+const Pool = pg.Pool
+const pool = new Pool(config)
+
+//Handle incorrect time
+pg.types.setTypeParser(1114, str => moment.utc(str).format());
+
+const vender_id = '1'
+const model_id = '0001'
+
+const extractJSON = (dataArray) => {
+    let locationsArray = []
+    let devices = []
+    let sequence = 0
+
+    for (let i = 0; i < dataArray.length; i++) {
+        // console.log(data[i].deviceid)
+        const data = dataArray[i]
+        const gps_model_id = (vender_id + model_id).padStart(7, '0')
+
+        const dupDevices = devices.filter(x => x.deviceid === data.deviceid && x.acctime === data.acctime)
+        if (dupDevices.length === 0) {
+            devices.push({
+                deviceid: data.deviceid,
+                acctime: data.acctime,
+                seq: 0
+            })
+        } else {
+            const findDevice = devices.find(x => x.deviceid === data.deviceid && x.acctime === data.acctime)
+            if (findDevice.seq < 65535)
+                findDevice.seq += 1
+            else
+                findDevice.seq = 0
+            sequence = findDevice.seq
+        }
+
+        dltjson = {
+            vender_id: vender_id,
+            locations_count: data.length,
+            unit_id: gps_model_id + data.deviceid.padStart(20, '0'),
+            driver_id: null,
+            seq: sequence,
+            utc_ts: data.gpstime,
+            recv_utc_ts: data.tstamp,
+            lat: data.lat,
+            lon: data.lon,
+            speed: data.speed,
+            engine_status: 1,
+            fix: 1,
+            license: null,
+            course: data.direction,
+            ext_power_status: 0,
+
+        }
+        locationsArray.push(dltjson)
+    }
+    return locationsArray
+}
+
+const getCarTrack = (request, response) => {
+    pool.query('SELECT * FROM cartrack ORDER BY deviceid asc LIMIT 100', (error, results) => {
+        if (error) {
+            throw error
+        }
+        // response.status(200).json(results.rows)
+        const locationsArray = extractJSON(results.rows)
+        // const unique = [...new Set(locationsArray.map(item => item.unit_id))];
+        response.status(200).json(locationsArray)
+        // postReq(locationsArray)
+    })
+}
+
+const postReq = (locationArray) => {
+    var postData = {
+        vender_id: 1,
+        locations_count: locationArray.length,
+        locations: locationArray
+    };
+
+    const fs = require('fs');
+    let data = JSON.stringify(postData);
+    fs.writeFileSync('dlt.json', data);
+}
+
+module.exports = {
+    getCarTrack,
+}
