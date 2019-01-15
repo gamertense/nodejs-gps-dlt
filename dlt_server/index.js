14b5b097551acec0ed05ad09cb8c7d9e9e23d229
Antonio Jack <ict2jack@gmail.com>
2019-01-16 00:20:55 +0700

    Create masterfile api at dlt side

--- dlt_server/index.js
+++ dlt_server/index.js
@@ -7,8 +7,13 @@ const moment = require('moment');
 const writeDLT = (postdata) => {
     const data = JSON.stringify(postdata);
     const time = moment().format('hh-mm');
-    console.log(time)
     fs.writeFileSync(`./temp_data/dlt${time}.json`, data);
+}
+
+const writeMaster = (postdata) => {
+    const data = JSON.stringify(postdata);
+    const time = moment().format('hh-mm');
+    fs.writeFileSync(`./masterfile/${time}.json`, data);
 }
 
 app.use(bodyParser.json())
@@ -24,6 +29,15 @@ app.post("/gps/add/locations", function (request, response) {
         code: 1,
         message: "ok",
         received_records: request.body.locations.length
+    });
+});
+
+app.post("/masterfile/add", function (request, response) {
+    writeMaster(request.body)
+    response.status(200).send({
+        code: 1,
+        message: "ok",
+        received_records: 1
     });
 });
 
