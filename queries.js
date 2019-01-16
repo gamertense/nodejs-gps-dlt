d1d7b74c366bf7f0af3f26ea7f139f42100d80de
Antonio Jack <ict2jack@gmail.com>
2019-01-17 00:30:45 +0700

    Add getDriverID()

--- queries.js
+++ queries.js
@@ -69,6 +69,24 @@ const extractJSON = (dataArray) => {
     return { devices, locationsArray }
 }
 
+const getDriverID = () => {
+    const dl3 = "+             4100            10            9999958  00100                     ?"
+    switch (dl3.length) {
+        case 77: // public transport
+            break;
+        case 80: //private car
+            let filtered = dl3.split(" ").filter(v => v != "")
+            filtered.splice(0, 1)  // Remove first element (+)
+            filtered.splice(-1, 1) // and last element (?) from array.
+            filtered[1] = filtered[1].slice(0, -1); // Remove 0 from 10
+            filtered[3] = filtered[3].slice(0, 3); // Remvoe last two 0's from 00100
+
+            return filtered.join('')
+        default:
+            console.log("Incorrect format!")
+    }
+}
+
 const getCarTrack = (request, response) => {
     // const q = `SELECT distinct on (cartrack.deviceid) cartrack.deviceid, *
     // FROM cars INNER JOIN cartrack on cars.idobd = cartrack.deviceid
